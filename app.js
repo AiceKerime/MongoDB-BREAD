012ffdd37b7a43edbde80375142276a0c69b77b1
AiceKerime <rafiizzaturohman06@gmail.com>
2022-10-18 22:46:19 +0700

    Belum connect DB

--- app.js
+++ app.js
@@ -3,39 +3,59 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const { MongoClient } = require('mongodb')
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-
-const app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+const uri = 'mongodb://localhost:27017'
+const dbName = 'users'
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+async function main() {
+  try {
+    await client.connect()
+    console.log('Conected successfully to server and database')
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+    const db = client.db(dbName)
 
-module.exports = app;
+    const indexRouter = require('./routes/index')(db);
+    const usersRouter = require('./routes/users');
+
+    const app = express();
+
+    // view engine setup
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'ejs');
+
+    app.use(logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, 'public')));
+
+    app.use('/', indexRouter);
+    app.use('/users', usersRouter);
+
+    // catch 404 and forward to error handler
+    app.use(function (req, res, next) {
+      next(createError(404));
+    });
+
+    // error handler
+    app.use(function (err, req, res, next) {
+      // set locals, only providing error in development
+      res.locals.message = err.message;
+      res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+      // render the error page
+      res.status(err.status || 500);
+      res.render('error');
+    });
+
+    module.exports = app;
+
+  } catch (error) {
+    throw 'Failed to Connect'
+  }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const { MongoClient } = require('mongodb')
-
-const url = 'mongodb://localhost:27017'
-const dbName = 'users'
-const client = new MongoClient(url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
-client.connect((error, client) => {
-  if (error) {
-    return console.log('Failed to connect!')
-  }
-
-  const db = client.db(dbName)
-
-  db.collection('dataBread').insertOne({
-    string: 'Coba Lagi',
-    integer: 99,
-    float: 100.405,
-    date: new Date('2017-11-20'),
-    boolean: false
-  }, (error, result) => {
-    if (error) {
-      console.error(error)
-    }
-    console.log(result)
-  })
-})
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
