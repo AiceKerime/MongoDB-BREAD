615e5d4e5487164fed7f149405b71db1428b733d
AiceKerime <rafiizzaturohman06@gmail.com>
2022-10-21 13:50:32 +0700

    SORTING NOT DONE YET

--- routes/index.js
+++ routes/index.js
@@ -8,14 +8,16 @@ module.exports = (db) => {
   // ROUTER GET DATA (VIEW)
   router.get('/', async (req, res) => {
     try {
-      const url = req.url == '/' ? '/?page=1' : req.url;
+      const url = req.url == '/' ? '/?page=1&sortBy=string&sortMode=asc' : req.url;
       const page = req.query.page || 1;
       const limit = 3;
       const offset = (page - 1) * limit;
       const wheres = {}
       const filter = `&idCheck=${req.query.idCheck}&id=${req.query.id}&stringCheck=${req.query.stringCheck}&string=${req.query.string}&integerCheck=${req.query.integerCheck}&integer=${req.query.integer}&floatCheck=${req.query.floatCheck}&float=${req.query.float}&dateCheck=${req.query.dateCheck}&startDate=${req.query.startDate}&endDate=${req.query.endDate}&booleanCheck=${req.query.booleanCheck}&boolean=${req.query.boolean}`
+
       var sortBy = req.query.sortBy == undefined ? 'string' : req.query.sortBy;
       var sortMode = req.query.sortMode == undefined ? 1 : req.query.sortMode;
+
       var sortMongo = JSON.parse(`{"${sortBy}" : ${sortMode}}`);
 
       if (req.query.string && req.query.stringCheck == 'on') {
